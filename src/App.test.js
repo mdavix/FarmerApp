685b594bcfd6889a0cf25c57d4889f2c505e0afb
Tobias Maritz <tmaritz@Tobiass-MacBook-Pro.local>
2020-09-09 11:51:20 +0100

    Added test for MAX_SAFE_INTEGER and refactored cost function to pass

--- src/App.test.js
+++ src/App.test.js
@@ -24,15 +24,21 @@ test('2 corn is 0.75', () => {
   expect(cost(2)).toBe(0.75);
 })
 
-test('3 corn is £1.25', () => {
+test('3 corn is 1.25', () => {
   expect(cost(3)).toBe(1.25);
 })
 
+test('MAX corn is 4503599627370495.25', () => {
+  expect(cost(Number.MAX_SAFE_INTEGER)).toBe(4503599627370495.25);
+})
+
 function cost(bagsOfCorn){
-  if(bagsOfCorn == 2)
+  if(bagsOfCorn == 0)
   {
-    return 0.75;
+    return 0.25;
   }
 
-  return 0.25;
+  const trips = (bagsOfCorn*2)-1;
+
+  return trips*0.25;
 }
