a215f1033c5c72ce3b8c46bd679dfd9841bab638
Tobias Maritz <tmaritz@Tobiass-MacBook-Pro.local>
2020-09-09 11:24:13 +0100

    Added tests for 0, 1, 2 bags of corn

--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,19 @@ test('renders learn react link', () => {
 test('sample test', () => {
   expect(true).toBe(true);
 });
+
+test('0 corn is £0.25', () => {
+  expect(cost(0)).toBe(0.25);
+})
+
+test('1 corn is £0.25', () => {
+  expect(cost(1)).toBe(0.25);
+})
+
+test('2 corn is £0.75', () => {
+  expect(cost(2)).toBe(0.75);
+})
+
+function cost(bagsOfCorn){
+  return 0.25;
+}
