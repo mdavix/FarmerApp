32741c3efeed8c6c6993445b98e71a706a4580c9
Matthew Davies <matthew.davies@madetech.com>
2020-09-09 11:33:51 +0100

    Added a test for 3 bags

--- src/App.test.js
+++ src/App.test.js
@@ -12,18 +12,27 @@ test('sample test', () => {
   expect(true).toBe(true);
 });
 
-test('0 corn is £0.25', () => {
+test('0 corn is 0.25', () => {
   expect(cost(0)).toBe(0.25);
 })
 
-test('1 corn is £0.25', () => {
+test('1 corn is 0.25', () => {
   expect(cost(1)).toBe(0.25);
 })
 
-test('2 corn is £0.75', () => {
+test('2 corn is 0.75', () => {
   expect(cost(2)).toBe(0.75);
 })
 
+test('3 corn is £1.25', () => {
+  expect(cost(3)).toBe(1.25);
+})
+
 function cost(bagsOfCorn){
+  if(bagsOfCorn == 2)
+  {
+    return 0.75;
+  }
+
   return 0.25;
 }
