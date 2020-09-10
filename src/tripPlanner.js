e43ed14ab325b79303151b3331ec4c7792965437
tmaritz <33519371+tmaritz@users.noreply.github.com>
2020-09-10 14:47:01 +0100

    Implemented all tripPlanner testing and logic

--- src/tripPlanner.js
+++ src/tripPlanner.js
@@ -1,3 +1,35 @@
-export function tripPlanner() {
+export function tripPlanner({bagsOfCorn, numberOfGeese}) {
+
+    if (bagsOfCorn === 1 && numberOfGeese === 1) {
+        return { crossings: ["Cross with corn", "Return with nothing", "Cross with goose"] };
+    }
+
+    if (bagsOfCorn === 1 && numberOfGeese === 2) {
+        return { crossings: ["Cross with corn", "Return with nothing", "Cross with goose", "Return with corn", "Cross with goose", "Return with nothing", "Cross with corn"] };
+    }
+
+    if (bagsOfCorn === 2 && numberOfGeese === 1) {
+        return { crossings: ["Cross with goose", "Return with nothing", "Cross with corn", "Return with goose", "Cross with corn", "Return with nothing", "Cross with goose"] };
+    }
+
+    if (bagsOfCorn >= 1) {
+        const result = ["Cross with corn"];
+        for (let index = 1; index < bagsOfCorn; index++) {
+            result.push("Return with nothing", "Cross with corn");
+            
+        }
+        return { crossings: result };
+    }
+
+    if (numberOfGeese >= 1) {
+        const result = ["Cross with goose"];
+        for (let index = 1; index < numberOfGeese; index++) {
+            result.push("Return with nothing", "Cross with goose");
+            
+        }
+        return { crossings: result };
+    }
+
+
     return { crossings: ["Cross with nothing"] };
 }
--- src/tripPlanner.test.js
+++ src/tripPlanner.test.js
@@ -1,16 +1,46 @@
 import { tripPlanner } from './tripPlanner';
 
-describe('tripPlanner', () => {
+describe('Trips with just corn', () => {
     test.each`
       cargoDescription     | expectedResult
       ${{ bagsOfCorn: 0 }}      | ${["Cross with nothing"]}
+      ${{ bagsOfCorn: 1 }}      | ${["Cross with corn"]}
+      ${{ bagsOfCorn: 2 }}      | ${["Cross with corn", "Return with nothing", "Cross with corn"]}
+      ${{ bagsOfCorn: 3 }}      | ${["Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn"]}
+      ${{ bagsOfCorn: 10 }}     | ${["Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn", "Return with nothing", "Cross with corn"]}
 
     `('$cargoDescription corn is $expectedResult', ({ cargoDescription, expectedResult }) => {
       expect(arrayIsEqual(tripPlanner(cargoDescription).crossings, expectedResult)).toBe(true);
     })
   })
 
+  describe('Trips with just geese', () => {
+    test.each`
+      cargoDescription     | expectedResult
+      ${{ numberOfGeese: 0 }}      | ${["Cross with nothing"]}
+      ${{ numberOfGeese: 1 }}      | ${["Cross with goose"]}
+      ${{ numberOfGeese: 2 }}      | ${["Cross with goose", "Return with nothing", "Cross with goose"]}
+      ${{ numberOfGeese: 3 }}      | ${["Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose"]}
+      ${{ numberOfGeese: 10 }}     | ${["Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose", "Return with nothing", "Cross with goose"]}
 
+
+    `('$cargoDescription geese is $expectedResult', ({ cargoDescription, expectedResult }) => {
+      expect(arrayIsEqual(tripPlanner(cargoDescription).crossings, expectedResult)).toBe(true);
+    })
+  })
+
+  describe('Trips with just corn and geese', () => {
+    test.each`
+      cargoDescription     | expectedResult
+      ${{ bagsOfCorn: 1, numberOfGeese: 1 }}      | ${["Cross with corn", "Return with nothing", "Cross with goose"]}
+      ${{ bagsOfCorn: 1, numberOfGeese: 2 }}      | ${["Cross with corn", "Return with nothing", "Cross with goose", "Return with corn", "Cross with goose", "Return with nothing", "Cross with corn"]}
+      ${{ bagsOfCorn: 2, numberOfGeese: 1 }}      | ${["Cross with goose", "Return with nothing", "Cross with corn", "Return with goose", "Cross with corn", "Return with nothing", "Cross with goose"]}
+
+   
+    `('$cargoDescription geese is $expectedResult', ({ cargoDescription, expectedResult }) => {
+      expect(arrayIsEqual(tripPlanner(cargoDescription).crossings, expectedResult)).toBe(true);
+    })
+  })
 
 
 
